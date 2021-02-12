76595f2cfb35f44026409f19910a0feea8cae83e
popsiclejokes <directwillm@gmail.com>
2021-02-12 12:27:07 -0500

    deleted:    .classpath 	deleted:    .gitignore 	deleted:    .project 	deleted:    .settings/org.eclipse.jdt.core.prefs 	new file:   SpaceRam/.gitignore 	new file:   SpaceRam/Assets/BackgroundLoop.cs 	new file:   SpaceRam/Assets/BackgroundLoop.cs.meta 	new file:   SpaceRam/Assets/CameraBehaviors.cs 	new file:   SpaceRam/Assets/CameraBehaviors.cs.meta 	new file:   SpaceRam/Assets/InputManager.cs 	new file:   SpaceRam/Assets/InputManager.cs.meta 	new file:   SpaceRam/Assets/Scenes.meta 	new file:   SpaceRam/Assets/Scenes/SampleScene.unity 	new file:   SpaceRam/Assets/Scenes/SampleScene.unity.meta 	new file:   SpaceRam/Assets/Starfield.mat 	new file:   SpaceRam/Assets/Starfield.mat.meta 	new file:   SpaceRam/Assets/seamless space.PNG 	new file:   SpaceRam/Assets/seamless space.PNG.meta 	new file:   SpaceRam/Packages/manifest.json 	new file:   SpaceRam/Packages/packages-lock.json 	new file:   SpaceRam/ProjectSettings/AudioManager.asset 	new file:   SpaceRam/ProjectSettings/ClusterInputManager.asset 	new file:   SpaceRam/ProjectSettings/DynamicsManager.asset 	new file:   SpaceRam/ProjectSettings/EditorBuildSettings.asset 	new file:   SpaceRam/ProjectSettings/EditorSettings.asset 	new file:   SpaceRam/ProjectSettings/GraphicsSettings.asset 	new file:   SpaceRam/ProjectSettings/InputManager.asset 	new file:   SpaceRam/ProjectSettings/NavMeshAreas.asset 	new file:   SpaceRam/ProjectSettings/NetworkManager.asset 	new file:   SpaceRam/ProjectSettings/Physics2DSettings.asset 	new file:   SpaceRam/ProjectSettings/PresetManager.asset 	new file:   SpaceRam/ProjectSettings/ProjectSettings.asset 	new file:   SpaceRam/ProjectSettings/ProjectVersion.txt 	new file:   SpaceRam/ProjectSettings/QualitySettings.asset 	new file:   SpaceRam/ProjectSettings/TagManager.asset 	new file:   SpaceRam/ProjectSettings/TimeManager.asset 	new file:   SpaceRam/ProjectSettings/UnityConnectSettings.asset 	new file:   SpaceRam/ProjectSettings/VFXManager.asset 	new file:   SpaceRam/ProjectSettings/VersionControlSettings.asset 	new file:   SpaceRam/ProjectSettings/XRSettings.asset 	deleted:    src/Untitled 2

---
+++ SpaceRam/Assets/InputManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+
+
+    Vector3 clickPoint;
+    float rotationSpeed;
+    float dashPower = 1000;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clickPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        rotationSpeed = 1440;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        clickPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButton(0))
+        {
+            var step = rotationSpeed * Time.deltaTime;
+            var rotationGoal = Quaternion.LookRotation(Vector3.forward, clickPoint - transform.position);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotationGoal, step);
+            rb.velocity -= (rb.velocity*4) * Time.deltaTime;
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            Debug.Log(transform.forward * dashPower);
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, clickPoint - transform.position);
+            rb.velocity = Vector2.zero;
+            rb.AddForce(transform.up * dashPower);
+        }
+
+        Camera camera = Camera.main;
+        float topBoundary = (camera.transform.position.y - camera.orthographicSize);
+        float bottomBoundary = (camera.transform.position.y + camera.orthographicSize);
+
+        if (transform.position.y < topBoundary || 
+            transform.position.y > bottomBoundary) {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z*-1);
+            transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, topBoundary, bottomBoundary));
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * -1);
+        }
+    }
+}
