92e54ca944806c4e191cc1401ee0f51298a20014
popsiclejokes <directwillm@gmail.com>
2021-02-22 19:01:20 -0500

    modified:   SpaceRam/Assets/Prefabs/enemy_turretship_small.prefab 	new file:   SpaceRam/Assets/Prefabs/light_bullet.prefab 	new file:   SpaceRam/Assets/Prefabs/light_bullet.prefab.meta 	new file:   SpaceRam/Assets/Prefabs/shoot_point.prefab 	new file:   SpaceRam/Assets/Prefabs/shoot_point.prefab.meta 	modified:   SpaceRam/Assets/Scenes/Debug_Level.unity 	new file:   SpaceRam/Assets/Scripts/LifeTime.cs 	new file:   SpaceRam/Assets/Scripts/LifeTime.cs.meta 	modified:   SpaceRam/Assets/Scripts/ShootingController.cs

--- SpaceRam/Assets/Scripts/ShootingController.cs
+++ SpaceRam/Assets/Scripts/ShootingController.cs
@@ -7,15 +7,19 @@ public class ShootingController : MonoBehaviour
 
     public GameObject projectile;
     public float frequency_seconds = 1;
-    public Transform Target;
+    //public Transform Target;
     private Vector2 direction;
     private float current_delay;
+    public float Force = 100;
 
     // Start is called before the first frame update
     void Start()
     {
         current_delay = frequency_seconds;
+        
     }
+
+    
 
     // Update is called once per frame
     void Update()
@@ -32,9 +36,23 @@ public class ShootingController : MonoBehaviour
 
     void FireProjectile()
     {
-        Vector2 targetPos = Target.position;
+        GameObject[] possibleTargets = GameObject.FindGameObjectsWithTag("Player");
+        GameObject closest = possibleTargets[0];
+        foreach (GameObject target in possibleTargets)
+        {
+
+            //float distance = Vector3.Distance(other.position, transform.position);
+            if (Vector3.Distance(target.transform.position, transform.position) < Vector3.Distance(closest.transform.position, transform.position))
+            {
+                closest = target;
+            }
+        }
+
+        Vector2 targetPos = closest.transform.position;
         direction = targetPos - (Vector2)transform.position;
 
+        GameObject newBullet = Instantiate(projectile, transform.position, Quaternion.LookRotation(Vector3.forward, direction));
+        newBullet.GetComponent<Rigidbody2D>().AddForce(direction.normalized * Force);
     }
 
     private void OnDrawGizmos()
