c0e5dba56457c683a759c8623b36fae47c144750
Kevin Diehr <kddiehr@gmail.com>
2021-04-11 13:56:29 -0400

    added boost object 	modified:   SpaceRam/Assets/Scenes/kevin_level_2-5.unity 	new file:   SpaceRam/Assets/Scripts/Environment/Boost.cs 	new file:   SpaceRam/Assets/Scripts/Environment/Boost.cs.meta

---
+++ SpaceRam/Assets/Scripts/Environment/Boost.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boost : MonoBehaviour
+{
+
+    public float boostAmt = 0;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if(collision.gameObject.tag == "Player")
+        {
+            // change momentum
+
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            rb.velocity = transform.up * boostAmt;
+        }
+    }
+}
