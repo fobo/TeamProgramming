c92a60976063b2b67388c31e8946c8d7aed27974
popsiclejokes <directwillm@gmail.com>
2021-04-11 14:23:59 -0400

    Boost arrow animation and boosting adjustments 	new file:   Resources/boostarrow.png 	new file:   SpaceRam/Assets/Prefabs/BoostArrow.prefab 	new file:   SpaceRam/Assets/Prefabs/BoostArrow.prefab.meta 	modified:   SpaceRam/Assets/Scenes/Level_1.unity 	modified:   SpaceRam/Assets/Scripts/Environment/Boost.cs 	new file:   SpaceRam/Assets/Sprites/BoostArrow.anim 	new file:   SpaceRam/Assets/Sprites/BoostArrow.anim.meta 	new file:   SpaceRam/Assets/Sprites/New Sprite.controller 	new file:   SpaceRam/Assets/Sprites/New Sprite.controller.meta 	new file:   SpaceRam/Assets/Sprites/boostarrow.png 	new file:   SpaceRam/Assets/Sprites/boostarrow.png.meta

--- SpaceRam/Assets/Scripts/Environment/Boost.cs
+++ SpaceRam/Assets/Scripts/Environment/Boost.cs
@@ -6,15 +6,34 @@ public class Boost : MonoBehaviour
 {
 
     public float boostAmt = 0;
+    private Rigidbody2D rb = null;
+    private bool playerIsColliding = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
         if(collision.gameObject.tag == "Player")
         {
-            // change momentum
-
-            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
-            rb.velocity = transform.up * boostAmt;
+            playerIsColliding = true;
+            rb = collision.gameObject.GetComponent<Rigidbody2D>();
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerIsColliding = false;
+            rb = null;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb != null && playerIsColliding == true)
+        {
+            rb.AddForce((Vector2)transform.right * boostAmt);
+        }
+        //rb.velocity = rb.velocity + (Vector2)transform.right * boostAmt;
     }
 }
