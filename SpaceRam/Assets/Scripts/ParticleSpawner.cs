3f84863cac6590e8bedf8ea0863bd3c0307ae04e
Kevin Diehr <kddiehr@gmail.com>
2021-04-13 12:12:42 -0400

    Added new projectile with cool smoke trail 	new file:   SpaceRam/Assets/Prefabs/SmokeAnim.prefab 	new file:   SpaceRam/Assets/Prefabs/SmokeAnim.prefab.meta 	modified:   SpaceRam/Assets/Prefabs/miniShip.prefab 	new file:   SpaceRam/Assets/Prefabs/missile1fix.prefab 	new file:   SpaceRam/Assets/Prefabs/missile1fix.prefab.meta 	modified:   SpaceRam/Assets/Scenes/kevin_level_3.unity 	new file:   SpaceRam/Assets/Scripts/AnimationAutoDestroy.cs 	new file:   SpaceRam/Assets/Scripts/AnimationAutoDestroy.cs.meta 	new file:   SpaceRam/Assets/Scripts/ParticleSpawner.cs 	new file:   SpaceRam/Assets/Scripts/ParticleSpawner.cs.meta 	new file:   SpaceRam/Assets/Sprites/GameObject (1).controller 	new file:   SpaceRam/Assets/Sprites/GameObject (1).controller.meta 	new file:   SpaceRam/Assets/Sprites/New Material.mat 	new file:   SpaceRam/Assets/Sprites/New Material.mat.meta 	new file:   SpaceRam/Assets/Sprites/Projectiles/missile1fix.png 	new file:   SpaceRam/Assets/Sprites/Projectiles/missile1fix.png.meta 	new file:   SpaceRam/Assets/Sprites/smokeTrail.anim 	new file:   SpaceRam/Assets/Sprites/smokeTrail.anim.meta 	new file:   SpaceRam/Assets/Sprites/smoketrail.png 	new file:   SpaceRam/Assets/Sprites/smoketrail.png.meta

---
+++ SpaceRam/Assets/Scripts/ParticleSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSpawner : MonoBehaviour
+{
+
+    //attach this script to a projectile or other object to have it spawn objects that play an animation
+
+
+
+    /*
+     * create object behind projectile
+     * 
+     * 
+     */
+
+    public float repeatDelay = 0.2f;
+    public float offset = 0.2f;
+    public GameObject spawnee; //drag and drop the prefab into this in the inspector
+    private float currDelay = 0f;
+    private void Update()
+    {
+        if (currDelay <= 0) {
+            currDelay = repeatDelay;
+            Vector2 spawnLocation = (Vector2)transform.position + (Vector2)transform.up * -1 * offset;
+            Instantiate(spawnee, spawnLocation, new Quaternion(0, 0, 0, 0));
+        }
+        else
+        {
+            currDelay -= Time.deltaTime;
+        }
+    }
+}
