cd11f8354ec225780398608237701d638b93897c
directwillm <directwillm@gmail.com>
2021-04-30 19:05:29 -0400

    Boost can now use a tag of arrays 	modified:   SpaceRam/Assets/Scripts/Environment/Boost.cs

--- SpaceRam/Assets/Scripts/Environment/Boost.cs
+++ SpaceRam/Assets/Scripts/Environment/Boost.cs
@@ -7,58 +7,74 @@ public class Boost : MonoBehaviour
 
     public float boostAmt = 0; //50 to 1 magnitude a frame
     public float boostMaxMagnitude = -1;
-    private Rigidbody2D rb = null;
+    private List<GameObject> gameObjectsArr = new List<GameObject>();
     private bool playerIsColliding = false;
+    public string[] effectsTagsArr = {"Player"};
+    private List<string> effectsTags = new List<string>();
+    private void Start()
+    {
+        foreach (string tag in effectsTagsArr)
+        {
+            effectsTags.Add(tag);
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         
-        if (collision.gameObject.tag == "Player")
+        if (effectsTags.Contains(collision.gameObject.tag))
         {
-            playerIsColliding = true;
-            rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            //playerIsColliding = true;
+            gameObjectsArr.Add(collision.gameObject);
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (effectsTags.Contains(collision.gameObject.tag))
         {
-            playerIsColliding = false;
-            rb = null;
+            //playerIsColliding = false;
+            gameObjectsArr.Remove(collision.gameObject);
         }
     }
 
     private void FixedUpdate()
     {
-        if (rb != null && playerIsColliding == true)
+
+        foreach (GameObject boostTarget in gameObjectsArr)
         {
-            if(boostMaxMagnitude == -1)
+            Rigidbody2D rb = boostTarget.GetComponent<Rigidbody2D>();
+            if (rb != null)
             {
-                rb.AddForce((Vector2)transform.right * boostAmt);
-
-            } else
-            {
-                Vector3 rbV = rb.velocity;
-                Vector3 rbVnorm = rb.velocity.normalized;
-                Vector3 correctDirNorm = transform.right;
-
-                float magInBoostDir = Vector3.Dot(rbV, correctDirNorm);
-                
-                Vector3 projection = magInBoostDir * rbVnorm;
-                Vector3 clampedProjection = Vector3.ClampMagnitude(projection, boostMaxMagnitude);
-                //Debug.Log(magInBoostDir); //50 on add force == 1 vel magnitude
-
-                if(magInBoostDir >= boostMaxMagnitude)
-                {
-                    rb.velocity = ((Vector3)rb.velocity - projection) + clampedProjection;
-                    return;
-                } else
+                if (boostMaxMagnitude == -1)
                 {
                     rb.AddForce((Vector2)transform.right * boostAmt);
-                }
-            }
 
+                }
+                else
+                {
+                    Vector3 rbV = rb.velocity;
+                    Vector3 rbVnorm = rb.velocity.normalized;
+                    Vector3 correctDirNorm = transform.right;
+
+                    float magInBoostDir = Vector3.Dot(rbV, correctDirNorm);
+
+                    Vector3 projection = magInBoostDir * rbVnorm;
+                    Vector3 clampedProjection = Vector3.ClampMagnitude(projection, boostMaxMagnitude);
+                    //Debug.Log(magInBoostDir); //50 on add force == 1 vel magnitude
+
+                    if (magInBoostDir >= boostMaxMagnitude)
+                    {
+                        rb.velocity = ((Vector3)rb.velocity - projection) + clampedProjection;
+                        return;
+                    }
+                    else
+                    {
+                        rb.AddForce((Vector2)transform.right * boostAmt);
+                    }
+                }
+
+            }
         }
         //rb.velocity = rb.velocity + (Vector2)transform.right * boostAmt;
     }
