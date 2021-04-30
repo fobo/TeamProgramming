afd32d2740a7b2ae63fb66422b9131f6d44d278b
Kevin Diehr <kddiehr@gmail.com>
2021-04-30 15:22:26 -0400

    added a song added enemy boosters (only works with momentum movetype) 	new file:   SpaceRam/Assets/Prefabs/BoostEnemy.prefab 	new file:   SpaceRam/Assets/Prefabs/BoostEnemy.prefab.meta 	new file:   SpaceRam/Assets/Resources/music/Alexander Brandon - Tyrian- Original Soundtrack - 25 Secret Level.mp3 	new file:   SpaceRam/Assets/Resources/music/Alexander Brandon - Tyrian- Original Soundtrack - 25 Secret Level.mp3.meta 	modified:   SpaceRam/Assets/Scenes/kevin_level_3.unity 	new file:   SpaceRam/Assets/Scripts/Environment/BoostEnemy.cs 	new file:   SpaceRam/Assets/Scripts/Environment/BoostEnemy.cs.meta

---
+++ SpaceRam/Assets/Scripts/Environment/BoostEnemy.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostEnemy : MonoBehaviour
+{
+
+    public float boostAmt = 0; //50 to 1 magnitude a frame
+    public float boostMaxMagnitude = -1;
+    private Rigidbody2D rb = null;
+    private bool playerIsColliding = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.tag == "Enemy")
+        {
+            playerIsColliding = true;
+            rb = collision.gameObject.GetComponent<Rigidbody2D>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            playerIsColliding = false;
+            rb = null;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb != null && playerIsColliding == true)
+        {
+            if(boostMaxMagnitude == -1)
+            {
+                rb.AddForce((Vector2)transform.right * boostAmt);
+
+            } else
+            {
+                Vector3 rbV = rb.velocity;
+                Vector3 rbVnorm = rb.velocity.normalized;
+                Vector3 correctDirNorm = transform.right;
+
+                float magInBoostDir = Vector3.Dot(rbV, correctDirNorm);
+                
+                Vector3 projection = magInBoostDir * rbVnorm;
+                Vector3 clampedProjection = Vector3.ClampMagnitude(projection, boostMaxMagnitude);
+                //Debug.Log(magInBoostDir); //50 on add force == 1 vel magnitude
+
+                if(magInBoostDir >= boostMaxMagnitude)
+                {
+                    rb.velocity = ((Vector3)rb.velocity - projection) + clampedProjection;
+                    return;
+                } else
+                {
+                    rb.AddForce((Vector2)transform.right * boostAmt);
+                }
+            }
+
+        }
+        //rb.velocity = rb.velocity + (Vector2)transform.right * boostAmt;
+    }
+}
